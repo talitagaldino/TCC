eeb788d7792ec031da56413e9514fa99795eb502
talitagaldino <talita.gouveia@ccc.ufcg.edu.br>
2022-07-26 15:08:30 -0300

    adiciona graficos dos tipos de cotas

--- cotas_ingressantes/cotas_ingressantes.R
+++ cotas_ingressantes/cotas_ingressantes.R
@@ -1,8 +1,16 @@
 DADOS_ALUNOS <- read.csv("./TCC/dadosfubica/anonymized_students.csv", sep = ";")
-dados_cadeiras <- read.csv("./TCC/dadosfubica/anonymized_enrollments.csv", sep = ";")
-library("dplyr")
 
-ggplot(DADOS_ALUNOS, aes(x=admissionYear,fill=affirmativePolicy))+
+library("dplyr")
+library("ggplot2")
+
+PRESENCIAL <- DADOS_ALUNOS %>% filter(admissionYear >= "2013.1", admissionYear < "2020") %>% 
+  filter(affirmativePolicy != "N/A") %>%
+  group_by(admissionYear, affirmativePolicy, gender) %>% summarise(quantidade = n())
+
+ggplot(PRESENCIAL, aes(x=admissionYear,fill=affirmativePolicy))+
   theme_bw()+
   facet_wrap(~gender)+
   geom_bar()
+
+ggplot(PRESENCIAL, aes(x=as.factor(admissionYear), y=quantidade, fill = affirmativePolicy)) +
+  geom_col() + coord_flip()
